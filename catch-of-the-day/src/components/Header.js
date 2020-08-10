dbe18c9a7ee0e65932f33e48976ebb550a41c573
Jacob Hensley <jacobrhensley3@gmail.com>
2020-08-10 15:40:44 -0500

    update header.js to render html using a function instead of render()

--- catch-of-the-day/src/components/Header.js
+++ catch-of-the-day/src/components/Header.js
@@ -1,21 +1,37 @@
 import React from "react";
 
-class Header extends React.Component {
-    render() {
-        return (
-            <header className="top">
-                <h1>Catch 
-                    <span className="ofThe">
-                        <span className="of">of</span>
-                        <span className="the">the</span>
-                    </span>
-                     Day</h1>
-                <h3 className="tagline">
-                    <span>{this.props.tagline}</span>
-                </h3>
-            </header>
-        )
-    }
+
+function Header(props) {
+    return (
+        <header className="top">
+            <h1>Catch 
+                <span className="ofThe">
+                    <span className="of">of</span>
+                    <span className="the">the</span>
+                </span>
+                 Day</h1>
+            <h3 className="tagline">
+                <span>{props.tagline}</span>
+            </h3>
+        </header>
+    )
 }
+// class Header extends React.Component {
+//     render() {
+//         return (
+//             <header className="top">
+//                 <h1>Catch 
+//                     <span className="ofThe">
+//                         <span className="of">of</span>
+//                         <span className="the">the</span>
+//                     </span>
+//                      Day</h1>
+//                 <h3 className="tagline">
+//                     <span>{this.props.tagline}</span>
+//                 </h3>
+//             </header>
+//         )
+//     }
+// }
 
 export default Header;
