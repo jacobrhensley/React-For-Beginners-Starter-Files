bef7df663c7f49fb4ceafed8a251268e6fc3b7f3
Jacob Hensley <jacobrhensley3@gmail.com>
2020-08-10 14:24:18 -0500

    learn JSX formatting and writing html in React render method

--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,2 +1,6 @@
-// let's go!
-alert('hello!');
+import React from 'react'; 
+import { render } from 'react-dom';
+import StorePicker from './components/StorePicker'
+
+
+render(<StorePicker/>, document.querySelector('#main'));
