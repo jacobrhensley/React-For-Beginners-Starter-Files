2f8f054ba66d99181504b42bc27093ffe97af0a1
Jacob Hensley <jacobrhensley3@gmail.com>
2020-08-10 16:02:52 -0500

    create Router and route existing views with proper URL

--- catch-of-the-day/src/components/Header.js
+++ catch-of-the-day/src/components/Header.js
@@ -16,22 +16,5 @@ function Header(props) {
         </header>
     )
 }
-// class Header extends React.Component {
-//     render() {
-//         return (
-//             <header className="top">
-//                 <h1>Catch 
-//                     <span className="ofThe">
-//                         <span className="of">of</span>
-//                         <span className="the">the</span>
-//                     </span>
-//                      Day</h1>
-//                 <h3 className="tagline">
-//                     <span>{this.props.tagline}</span>
-//                 </h3>
-//             </header>
-//         )
-//     }
-// }
 
 export default Header;
