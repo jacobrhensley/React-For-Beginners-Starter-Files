24c60009b8a640b58fc1b24ef64bd1a03e62fd73
Jacob Hensley <jacobrhensley3@gmail.com>
2020-08-11 17:20:57 -0500

    Display fish choices under Header section of store page using JSX and this.props.details

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -3,6 +3,7 @@ import Header from "./Header"
 import Inventory from "./Inventory"
 import Order from "./Order"
 import sampleFishes from "../sample-fishes"
+import Fish from "./Fish"
 
 class App extends React.Component {
     state = {
@@ -27,6 +28,9 @@ class App extends React.Component {
             <div className="catch-of-the-day">
                 <div className="menu">
                     <Header tagline="Fresh Seafood Market"/>
+                    <ul className="fishes">
+        {Object.keys(this.state.fishes).map(key => <Fish key={key} details={this.state.fishes[key]} />)}
+                    </ul>
                 </div>
                 <Order/>
                 <Inventory 
