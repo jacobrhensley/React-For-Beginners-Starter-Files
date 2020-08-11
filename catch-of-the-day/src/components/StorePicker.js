a5a7c458372bd796bf7849073cf602ae6f68a9e2
Jacob Hensley <jacobrhensley3@gmail.com>
2020-08-10 21:54:20 -0500

    complete redirect to store page onSubmit using props.history

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -4,24 +4,24 @@ import {getFunName} from "../helpers"
 class StorePicker extends React.Component {
 myInput = React.createRef();
 
-    goToStore = (event) => {
+    goToStore = event => {
         // 1. Stop form from submitting
         event.preventDefault();
         // 2. get the text from the form
-        console.log(this);
+        const storeName = this.myInput.current.value;
         // 3. change the page to /store/whatever-they-entered
-
+        this.props.history.push(`/store/${storeName}`)
     }
-    myInput = React.createRef;
+
     render(){
         return (
             <form className="store-selector" onSubmit={this.goToStore}>
                <h2>Please Enter A Store</h2>
                <input
-                type="text"
-                required placeholder="Store Name"
-                defaultValue={getFunName()}
-                ref={this.myInput}
+                    type="text"
+                    ref={this.myInput}
+                    required placeholder="Store Name"
+                    defaultValue={getFunName()}
                 />
                 <button type="submit">Visit Store</button>
             </form>
