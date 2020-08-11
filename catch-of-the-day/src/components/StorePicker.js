c9ce66a9dd1c5cb6197d87c0941cf2ab7aaa5fa6
Jacob Hensley <jacobrhensley3@gmail.com>
2020-08-10 21:11:50 -0500

    create structure for button to submit redirect to goToStore. Learned onSubmit inline method and ref attribute

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -1,15 +1,30 @@
 import React from 'react';
+import {getFunName} from "../helpers"
 
 class StorePicker extends React.Component {
+myInput = React.createRef();
+
+    goToStore = (event) => {
+        // 1. Stop form from submitting
+        event.preventDefault();
+        // 2. get the text from the form
+        console.log(this);
+        // 3. change the page to /store/whatever-they-entered
+
+    }
+    myInput = React.createRef;
     render(){
         return (
-        <React.Fragment>
-            <form className="store-selector">
+            <form className="store-selector" onSubmit={this.goToStore}>
                <h2>Please Enter A Store</h2>
-               <input type="text" required placeholder="Store Name"/>
+               <input
+                type="text"
+                required placeholder="Store Name"
+                defaultValue={getFunName()}
+                ref={this.myInput}
+                />
                 <button type="submit">Visit Store</button>
             </form>
-        </React.Fragment>
         )
     }
 }
