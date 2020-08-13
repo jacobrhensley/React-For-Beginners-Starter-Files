6ea11e6e23e18b887d7774069d50150f8eafa7ac
Jacob Hensley <jacobrhensley3@gmail.com>
2020-08-13 15:58:44 -0500

    setup firebase and connect it to the app. Issue syncing firebase with my import reaching out for help

---
+++ catch-of-the-day/src/base.js
@@ -0,0 +1,16 @@
+import Rebase from 're-base';
+import firebase from 'firebase';
+
+ const firebaseApp = firebase.initializeApp({
+        apiKey: "AIzaSyCY861ki_D7cSu_VTb2qHgAg_Q9CMknYi0",
+        authDomain: "catch-of-the-day-jacob-hensley.firebaseapp.com",
+        databaseURL: "https://catch-of-the-day-jacob-hensley.firebaseio.com"
+      })
+
+      const base = Rebase.createClass(firebaseApp.database());
+
+    //   This is a named export 
+      export { firebaseApp };
+
+    //   This is a default export 
+    export default base;
--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -4,12 +4,20 @@ import Inventory from "./Inventory"
 import Order from "./Order"
 import sampleFishes from "../sample-fishes"
 import Fish from "./Fish"
-
+import base from "../base"
 class App extends React.Component {
     state = {
         fishes: {},
         order: {},
     };
+    componentDidMount(){
+        const { params } = this.props.match;
+        this.ref = base.syncState(`${params.storeId}/fishes`, {
+            context: this,
+            state: 'fishes'
+        });
+    }
+
     addFish = fish => {
         // 1. Take a copy of the existing state
         const fishes = {...this.state.fishes }
@@ -48,7 +56,7 @@ class App extends React.Component {
         ))}
                     </ul>
                 </div>
-                <Order/>
+                <Order fishes={this.state.fishes} order={this.state.order}/>
                 <Inventory 
                 addFish={this.addFish}
                 loadSampleFishes={this.loadSampleFishes} 
