5235ae9ee6ebf07be4dffbc851e82acb0cda3cf1
Jacob Hensley <jacobrhensley3@gmail.com>
2020-08-13 12:49:14 -0500

    added background logic to update order state and create object items for us to grab

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -23,13 +23,29 @@ class App extends React.Component {
     loadSampleFishes = () => {
         this.setState({ fishes: sampleFishes });
     };
+
+    addToOrder = (key) => {
+        // 1. take a copy of state 
+        const order = { ...this.state.order };
+        // 2. either add to the order, or update the number in our order
+        order[key] = order[key] + 1 || 1;
+        // 3. call setState to update our state object
+        this.setState({ order });
+    }
     render() {
         return (
             <div className="catch-of-the-day">
                 <div className="menu">
                     <Header tagline="Fresh Seafood Market"/>
                     <ul className="fishes">
-        {Object.keys(this.state.fishes).map(key => <Fish key={key} details={this.state.fishes[key]} />)}
+        {Object.keys(this.state.fishes).map(key => (
+        <Fish 
+        key={key} 
+        index={key}
+        details={this.state.fishes[key]} 
+        addToOrder = {this.addToOrder}
+        />
+        ))}
                     </ul>
                 </div>
                 <Order/>
